ee5231b43ffd7b973603d6884fee0384e44c20d3
Jung-Hey <gjgmlwjd9@gmail.com>
2024-11-06 16:25:49 +0900

    test

---
+++ src/main/java/com/multitab/bookingSchedule/BookingScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.multitab.bookingSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multitab/bookingSchedule/BookingScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multitab.bookingSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
